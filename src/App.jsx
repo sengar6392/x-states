ae81e61fa561117850b8e240e84dd8ab89cad0ef
Shivam Sengar <shivamsengar995627@gmail.com>
2023-12-24 11:28:39 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
 
+  const handleCountryChange = (e) => {
+    setSelectedState("");
+    setCities([]);
+    setSelectedCity("");
+    setSelectedCountry(e.target.value);
+  };
+  const handleStateChange = (e) => {
+    setSelectedCity("");
+    setSelectedState(e.target.value);
+  };
+  const handleCityChange = (e) => {
+    setSelectedCity(e.target.value);
+  };
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch(
+        "https://crio-location-selector.onrender.com/countries"
+      );
+      let data = await response.json();
+      data =new Set(data);
+      data = Array.from(data);
+      setCountries(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchStates = async () => {
+    try {
+      const response = await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/states`
+      );
+      const data = await response.json();
+      setStates(data);
+      // setSelectedState("")
+      // setCities([])
+      // setSelectedCity("")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchCities = async () => {
+    try {
+      const response = await fetch(
+        `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${selectedState}/cities`
+      );
+      const data = await response.json();
+      setCities(data);
+      // setSelectedCity("")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+  useEffect(() => {
+    if (selectedCountry) fetchStates();
+  }, [selectedCountry]);
+
+  useEffect(() => {
+    if (selectedState && selectedCountry) fetchCities();
+  }, [selectedState, selectedCountry]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <select
+        name="countries"
+        id=""
+        value={selectedCountry}
+        onChange={handleCountryChange}
+        disabled={!countries.length && true}
+      >
+        <option value="" disabled>
+          Select Country
+        </option>
+        {countries.map((country) => (
+          <option value={country} key={country}>
+            {country}
+          </option>
+        ))}
+      </select>
+      <select
+        name="states"
+        id=""
+        value={selectedState}
+        onChange={handleStateChange}
+        disabled={!states.length && true}
+      >
+        <option value="" disabled>
+          Select State
+        </option>
+        {states.map((state) => (
+          <option value={state} key={state}>
+            {state}
+          </option>
+        ))}
+      </select>
+      <select
+        name="cities"
+        id=""
+        value={selectedCity}
+        onChange={handleCityChange}
+        disabled={!cities.length && true}
+      >
+        <option value="" disabled>
+          Select City
+        </option>
+        {cities.map((city) => (
+          <option value={city} key={city}>
+            {city}
+          </option>
+        ))}
+      </select>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
